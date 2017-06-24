b6b7f6baa02ae6f93dce82b75b1b5105dec85fba
gowripurushothaman <gowrikalpana27@gmail.com>
2017-06-24 14:01:16 +0530

    Create positivenegative.c

---
+++ positivenegative.c
@@ -0,0 +1,12 @@
+int number;
+printf("enter a number :");
+scanf("%d",&number);
+if (number<=0)
+{
+if (number==0)
+printf("you entered 0");
+else
+printf("you entered a negative number");
+}
+else
+printf("entered a positive number");
